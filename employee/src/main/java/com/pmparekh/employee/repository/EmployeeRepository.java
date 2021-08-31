d386848a82de8f20eac7338302f0a11ca632a72e
Pmparekh2000 <pmp.tech2000@gmail.com>
2021-08-31 09:05:00 +0530

    Added custom exception, tested all the API's

--- employee/src/main/java/com/pmparekh/employee/repository/EmployeeRepository.java
+++ employee/src/main/java/com/pmparekh/employee/repository/EmployeeRepository.java
@@ -1,9 +1,12 @@
 package com.pmparekh.employee.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pmparekh.employee.model.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
+	void deleteEmployeeById(Long id);
+	Optional<Employee> findEmployeeById(Long id);
 }
--- employee/src/main/java/com/pmparekh/employee/service/EmployeeService.java
+++ employee/src/main/java/com/pmparekh/employee/service/EmployeeService.java
@@ -1,10 +1,14 @@
 package com.pmparekh.employee.service;
 
+import java.util.List;
 import java.util.UUID;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.pmparekh.employee.exceptions.UserNotFoundException;
 import com.pmparekh.employee.model.Employee;
 import com.pmparekh.employee.repository.EmployeeRepository;
 
@@ -22,4 +26,25 @@ public class EmployeeService {
 		employee.setEmployeeCode(UUID.randomUUID().toString());
 		return employeeRepository.save(employee);
 	}
+	
+	public List<Employee> findAllEmployee() {
+		return employeeRepository.findAll();
+	}
+	
+	public Employee updateEmployee(Employee employee) {
+		return employeeRepository.save(employee);
+	}
+	
+	public Employee findEmployeeById(Long id) {
+		// This is known as query method since spring doesn't have a default method like this.
+		// We need to define it in t the repository by ourselfs
+		return employeeRepository.findEmployeeById(id)
+				.orElseThrow(() -> new UserNotFoundException("User by id "+id+" was not found"));
+	}
+	
+	@Transactional
+	public void deleteEmployee(Long id) {
+		employeeRepository.deleteEmployeeById(id);
+	}
+	
 }
